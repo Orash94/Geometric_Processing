f2ba4e479d67a04fd1a663c9c4425982c489be1b
Orash94 <orash94@gmail.com>
2021-05-03 20:12:01 +0300

    Assignment 3 Submission

--- assignment3/README.md
+++ assignment3/README.md
@@ -1,8 +1,294 @@
 # Assignment 3
 
-Edit this 'README.md' file to report all your results. There is no need to write lengthy reports, just show the requested outputs and screenshots and quickly summarize your observations. Please add your additional files or notes in the folder 'assignment3/results' and refer to or directly show them in this page.
+
 
 ## Required results
 
-* Screenshots of the parameterizations and textured (checkerboard) models for all the implemented methods and boundary conditions (models: cathead.obj, hemisphere.off, hemisphere_non_convex_boundary.off,Octo_cut2.obj)
-* Several examples of the distortion visualizations.
+### Hemishpere
+
+Uniform Laplacian (3D):
+
+![hemisphere uniform laplacian](photos/hemisphere uniform laplacian.PNG)
+
+
+
+Uniform Laplacian (UV representation):
+
+![hemisphere uniform laplacian parametrization](photos/hemisphere uniform laplacian parametrization.PNG)
+
+
+
+Cotangent Laplacian (3D):
+
+![hemisphere cotangent laplacian](photos/hemisphere cotangent laplacian.PNG)
+
+
+
+Uniform Laplacian (UV representation):
+
+![hemisphere cotangent laplacian parametrization](photos/hemisphere cotangent laplacian parametrization.PNG)
+
+
+
+LSCM (3D):
+
+![hemisphere LSCM](photos/hemisphere LSCM.PNG)
+
+
+
+LSCM (UV representation):
+
+![hemisphere LSCM Parametrization](photos/hemisphere LSCM Parametrization.PNG)
+
+
+
+
+
+
+
+
+
+### Measuring the distortions (Isometric & Conformal) for all parametrization methods
+
+
+
+Uniform Laplacian & Conformal Distortion
+
+<img src="photos/hemisphere uniform laplacian parametrization angle preserve.PNG" alt="hemisphere uniform laplacian parametrization angle preserve" style="zoom: 33%;" /> <img src="photos/hemisphere uniform laplacian angle preserve.PNG" alt="hemisphere uniform laplacian angle preserve" style="zoom: 33%;" />
+
+
+
+Uniform Laplacian & Isometric Distortion
+
+<img src="photos/hemisphere uniform laplacian parametrization edge length preserve.PNG" alt="hemisphere uniform laplacian parametrization edge length preserve" style="zoom:33%;" /> <img src="photos/hemisphere uniform laplacian edge length preserve.PNG" alt="hemisphere uniform laplacian edge length preserve" style="zoom:33%;" />
+
+
+
+
+
+
+
+Cotangent Laplacian & Conformal Distortion
+
+<img src="photos/hemisphere cotangent laplacian parametrization angle preserve.PNG" alt="hemisphere cotangent laplacian parametrization angle preserve" style="zoom:33%;" /> <img src="photos/hemisphere cotangent laplacian angle preserve.PNG" alt="hemisphere cotangent laplacian angle preserve" style="zoom:33%;" />
+
+
+
+Cotangent Laplacian & Isometric Distortion
+
+<img src="photos/hemisphere cotangent laplacian parametrization edge length preserve.PNG" alt="hemisphere cotangent laplacian parametrization edge length preserve" style="zoom:33%;" /> <img src="photos/hemisphere cotangent laplacian edge length preserve.PNG" alt="hemisphere cotangent laplacian edge length preserve" style="zoom:33%;" />
+
+
+
+
+
+
+
+LSCM & Conformal Distortion
+
+<img src="photos/hemisphere LSCM Parametrization angle preserve.PNG" alt="hemisphere LSCM Parametrization angle preserve" style="zoom:33%;" /> <img src="photos/hemisphere LSCM angle preserve.PNG" alt="hemisphere LSCM angle preserve" style="zoom:33%;" />
+
+
+
+LSCM &  Isometric Distortion
+
+<img src="photos/hemisphere LSCM Parametrization edge length preserve.PNG" alt="hemisphere LSCM Parametrization edge length preserve" style="zoom:33%;" /> <img src="photos/hemisphere LSCM edge length preserve.PNG" alt="hemisphere LSCM edge length preserve" style="zoom:33%;" /> 
+
+
+
+
+
+
+
+### Observations:
+
+I chose to demonstrate the hemisphere object first because we can see the the direct consequences of the different parametrization methods, since the hemisphere object is very intuitive when we think of parametrization.
+
+When we look at the conformal distortion over the three parametrization methods, we can see that the distortion of the Uniform Laplacian is more scattered over the object (based on the UV representation ) other than the other two, which we can see that the distortion resides in he sidelines of the UV representation (the hemisphere's bottom). 
+When we speak about the other two (Cotangent Laplacian and LSCM), we can see that the distortion is pretty much in the same places while the LSCM method brigs us less distorted results, as expected.
+
+ When we look at the isometric distortion over the three parametrization methods, we can see that the distortion of the LSCM is more dense  in the middle of the parametrization than the other two, but in the same time has strong distortions. Among the other two Laplacians we can see that the distortion is more scattered,  but relatively weaker than the LSCM. 
+
+
+
+
+
+
+
+
+
+
+## More Results
+
+
+
+## cathead
+
+Uniform Laplacian (3D):
+
+![cathead uniform laplacian](photos/cathead uniform laplacian.PNG)
+
+
+
+Uniform Laplacian (UV):
+
+![cathead uniform laplacian UV](photos/cathead uniform laplacian UV.PNG)
+
+
+
+Cotangent Laplacian (3D):
+
+![cathead cotangent laplacian](photos/cathead cotangent laplacian.PNG)
+
+
+
+Cotangent Laplacian (UV):
+
+![cathead cotangent laplacian UV](photos/cathead cotangent laplacian UV.PNG)
+
+
+
+LSCM (3D):
+
+![cathead LSCM](photos/cathead LSCM.PNG)
+
+
+
+LSCM (UV):
+
+![cathead LSCM UV](photos/cathead LSCM UV.PNG)
+
+
+
+
+
+### Measuring the distortions (Isometric & Conformal) for all parametrization methods
+
+Uniform Laplacian & Conformal Distortion
+
+<img src="photos/cathead uniform laplacian Angle.PNG" alt="cathead uniform laplacian Angle" style="zoom:33%;" /> <img src="photos/cathead uniform laplacian UV angle.PNG" alt="cathead uniform laplacian UV angle" style="zoom:33%;" />
+
+
+
+Uniform Laplacian & Isometric Distortion
+
+<img src="photos/cathead uniform laplacian Edge Length.PNG" alt="cathead uniform laplacian Edge Length" style="zoom:33%;" /> <img src="photos/cathead uniform laplacian UV Edge Length.PNG" alt="cathead uniform laplacian UV Edge Length" style="zoom:33%;" />
+
+
+
+Cotangent Laplacian & Conformal Distortion
+
+<img src="photos/cathead cotangent laplacian Angle.PNG" alt="cathead cotangent laplacian Angle" style="zoom:33%;" /> <img src="photos/cathead cotangent laplacian UV Angle.PNG" alt="cathead cotangent laplacian UV Angle" style="zoom:33%;" /> 
+
+
+
+Cotangent Laplacian & Isometric Distortion
+
+<img src="photos/cathead cotangent laplacian Edge Length.PNG" alt="cathead cotangent laplacian Edge Length" style="zoom:33%;" /> <img src="photos/cathead cotangent laplacian UV Edge Length.PNG" alt="cathead cotangent laplacian UV Edge Length" style="zoom:33%;" /> 
+
+
+
+
+
+LSCM & Conformal Distortion
+
+<img src="photos/cathead LSCM Angle.PNG" alt="cathead LSCM Angle" style="zoom:33%;" /> <img src="photos/cathead LSCM UV Angle.PNG" alt="cathead LSCM UV Angle" style="zoom:33%;" /> 
+
+
+
+LSCM & Isometric Distortion
+
+<img src="photos/cathead LSCM Edge Length.PNG" alt="cathead LSCM Edge Length" style="zoom:33%;" /> <img src="photos/cathead LSCM UV Edge Length.PNG" alt="cathead LSCM UV Edge Length" style="zoom:33%;" /> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Bunny
+
+Uniform Laplacian (3D):
+
+![bunny uniform laplacian](photos/bunny uniform laplacian.PNG) 
+
+
+
+Uniform Laplacian (UV):
+
+![bunny uniform laplacian UV](photos/bunny uniform laplacian UV.PNG)
+
+
+
+Cotangent Laplacian (3D):
+
+![bunny cotangent laplacian](photos/bunny cotangent laplacian.PNG)
+
+Uniform Laplacian (UV):
+
+![bunny cotangent laplacian UV](photos/bunny cotangent laplacian UV.PNG)
+
+
+
+LSCM (3D):
+
+![bunny LSCM](photos/bunny LSCM.PNG)
+
+
+
+Uniform Laplacian (UV):
+
+![bunny LSCM UV](photos/bunny LSCM UV.PNG)
+
+
+
+
+
+
+
+### Measuring the distortions (Isometric & Conformal) for all parametrization methods
+
+
+
+Uniform Laplacian & Conformal Distortion
+
+<img src="photos/bunny uniform laplacian Angle.PNG" alt="bunny uniform laplacian Angle" style="zoom:33%;" /> <img src="photos/bunny uniform laplacian UV Angle.PNG" alt="bunny uniform laplacian UV Angle" style="zoom:33%;" /> 
+
+
+
+Uniform Laplacian & Isometric Distortion
+
+<img src="photos/bunny uniform laplacian Edge Length.PNG" alt="bunny uniform laplacian Edge Length" style="zoom:33%;" /> <img src="photos/bunny uniform laplacian UV Edge Length.PNG" alt="bunny uniform laplacian UV Edge Length" style="zoom:33%;" /> 
+
+
+
+Cotangent Laplacian & Conformal Distortion
+
+<img src="photos/bunny cotangent laplacian Angle.PNG" alt="bunny cotangent laplacian Angle" style="zoom:33%;" /> <img src="photos/bunny cotangent laplacian UV Angle.PNG" alt="bunny cotangent laplacian UV Angle" style="zoom:33%;" /> 
+
+
+
+Cotangent Laplacian & Isometric Distortion
+
+<img src="photos/bunny cotangent laplacian Edge Length.PNG" alt="bunny cotangent laplacian Edge Length" style="zoom:33%;" /> <img src="photos/bunny cotangent laplacian UV Edge Length.PNG" alt="bunny cotangent laplacian UV Edge Length" style="zoom:33%;" /> 
+
+
+
+LSCM & Conformal Distortion
+
+<img src="photos/bunny LSCM Angle.PNG" alt="bunny LSCM Angle" style="zoom:33%;" /> <img src="photos/bunny LSCM UV Angle.PNG" alt="bunny LSCM UV Angle" style="zoom:33%;" /> 
+
+
+
+
+
+LSCM & Isometric Distortion
+
+<img src="photos/bunny LSCM Edge Length.PNG" alt="bunny LSCM Edge Length" style="zoom:33%;" /> <img src="photos/bunny LSCM UV Edge Length.PNG" alt="bunny LSCM UV Edge Length" style="zoom:33%;" /> 
+
--- assignment3/src/main.cpp
+++ assignment3/src/main.cpp
@@ -22,6 +22,7 @@
 #include <igl/speye.h>
 #include <igl/repdiag.h>
 #include <igl/cat.h>
+#include <igl/dijkstra.h>
 
 using namespace std;
 using namespace Eigen;
@@ -38,11 +39,20 @@ Eigen::MatrixXi F;
 // UV coordinates, #V x2
 Eigen::MatrixXd UV;
 
+
+Eigen::VectorXd distortion; // Vector that represents the distortion size for each face, #F x 1
+
+Eigen::MatrixXd distortionColors; // matrix for the distortion colors, #F x 3
+
 bool showingUV = false;
 bool freeBoundary = false;
 double TextureResolution = 10;
+int distortionType = -1;
 igl::opengl::ViewerCore temp3D;
 igl::opengl::ViewerCore temp2D;
+bool angle = false;
+bool edge = false;
+
 
 void Redraw()
 {
@@ -64,6 +74,7 @@ void Redraw()
 		viewer.data().show_texture = false;
 		viewer.data().set_mesh(UV, F);
 	}
+	viewer.data().set_colors(distortionColors);
 }
 
 bool callback_mouse_move(Viewer &viewer, int mouse_x, int mouse_y)
@@ -80,14 +91,90 @@ static void computeSurfaceGradientMatrix(SparseMatrix<double> & D1, SparseMatrix
 
 	igl::local_basis(V, F, F1, F2, F3);
 	igl::grad(V, F, DD);
-
 	Dx = DD.topLeftCorner(F.rows(), V.rows());
 	Dy = DD.block(F.rows(), 0, F.rows(), V.rows());
 	Dz = DD.bottomRightCorner(F.rows(), V.rows());
-
 	D1 = F1.col(0).asDiagonal()*Dx + F1.col(1).asDiagonal()*Dy + F1.col(2).asDiagonal()*Dz;
 	D2 = F2.col(0).asDiagonal()*Dx + F2.col(1).asDiagonal()*Dy + F2.col(2).asDiagonal()*Dz;
 }
+
+
+void evaluateDistortion(int type) {
+
+	Eigen::SparseMatrix<double> Dx, Dy;
+	Eigen::MatrixXd J1, J2, J3, J4, J, D;
+
+	distortionColors.conservativeResize(F.rows(), 3);
+	J.conservativeResize(2, 2);
+	distortion.conservativeResize(F.rows());
+	computeSurfaceGradientMatrix(Dx, Dy);
+
+	J1 = Dx * UV.col(0);
+	J2 = Dx * UV.col(1);
+	J3 = Dy * UV.col(0);
+	J4 = Dy * UV.col(1);
+
+
+	switch (type) {
+	case 0:
+	{
+		Eigen::MatrixXd I;
+		I = Eigen::MatrixXd::Identity(2, 2);
+
+		for (int i = 0; i < F.rows(); i++) {
+			J(0, 0) = J1(i, 0);
+			J(0, 1) = J2(i, 0);
+			J(1, 0) = J3(i, 0);
+			J(1, 1) = J4(i, 0);
+			D = J + J.transpose() - J.trace() * I;
+			distortion[i] = pow(D.norm(), 2);
+		}
+		break;
+	}
+	case 1:
+	{
+		Eigen::MatrixXd U, V, R, UVT;
+		R.conservativeResize(2, 2);
+
+		for (int i = 0; i < F.rows(); i++) {
+			J(0, 0) = J1(i, 0);
+			J(0, 1) = J2(i, 0);
+			J(1, 0) = J3(i, 0);
+			J(1, 1) = J4(i, 0);
+			JacobiSVD<MatrixXd> svd(J, ComputeThinU | ComputeThinV);
+			U = svd.matrixU();
+			V = svd.matrixV();
+			UVT = U * V.transpose();
+			R(0, 0) = 1;
+			R(0, 1) = 0;
+			R(1, 0) = 0;
+			if (UVT.norm() == 0)
+				R(1, 1) = 0;
+			else
+			{
+				R(1, 1) = 1;
+			}
+			R = U * R * V.transpose();
+			D = J - R;
+			distortion[i] = pow(D.norm(), 2);
+		}
+		break;
+	}
+	default:
+	{
+		for (int i = 0; i < F.rows(); i++) {
+			J(0, 0) = J1(i, 0);
+			J(0, 1) = J2(i, 0);
+			J(1, 0) = J3(i, 0);
+			J(1, 1) = J4(i, 0);
+
+			distortion[i] = pow(J(0, 0) * J(1, 1) - J(1, 0) * J(0, 1) - 1, 2);
+		}
+	}
+
+	}
+}
+
 static inline void SSVD2x2(const Eigen::Matrix2d& J, Eigen::Matrix2d& U, Eigen::Matrix2d& S, Eigen::Matrix2d& V)
 {
 	double e = (J(0) + J(3))*0.5;
@@ -121,10 +208,23 @@ static inline void SSVD2x2(const Eigen::Matrix2d& J, Eigen::Matrix2d& U, Eigen::
 	V(3) = c;
 }
 
+
+
 void ConvertConstraintsToMatrixForm(VectorXi indices, MatrixXd positions, Eigen::SparseMatrix<double> &C, VectorXd &d)
 {
 	// Convert the list of fixed indices and their fixed positions to a linear system
 	// Hint: The matrix C should contain only one non-zero element per row and d should contain the positions in the correct order.
+	Eigen::SparseMatrix<double> c;
+	c.resize(indices.rows(), V.rows());
+	C.resize(2 * indices.rows(), 2 * V.rows());
+	d.resize(2 * indices.rows(), 1);
+	d = (Map<VectorXd>(positions.data(), positions.cols() * positions.rows()));
+	for (int i = 0; i < indices.rows(); i++) {
+		int index = indices(i, 0);
+		c.insert(i, index) = 1;
+	}
+
+	igl::repdiag(c, 2, C);
 }
 
 void computeParameterization(int type)
@@ -132,19 +232,46 @@ void computeParameterization(int type)
 	VectorXi fixed_UV_indices;
 	MatrixXd fixed_UV_positions;
 
-	SparseMatrix<double> A;
-	VectorXd b;
-	Eigen::SparseMatrix<double> C;
-	VectorXd d;
+	VectorXd b, d;
+	Eigen::SparseMatrix<double>A, C;
 	// Find the indices of the boundary vertices of the mesh and put them in fixed_UV_indices
 	if (!freeBoundary)
 	{
 		// The boundary vertices should be fixed to positions on the unit disc. Find these position and
 		// save them in the #V x 2 matrix fixed_UV_position.
+		igl::boundary_loop(F, fixed_UV_indices);
+		igl::map_vertices_to_circle(V, fixed_UV_indices, fixed_UV_positions);
+
 	}
 	else
 	{
 		// Fix two UV vertices. This should be done in an intelligent way. Hint: The two fixed vertices should be the two most distant one on the mesh.
+		vector<vector<int>> VV;
+		igl::adjacency_list(F, VV);
+
+		double max_distance = numeric_limits<double>::min();
+		int firstIndex, secondIndex;
+		firstIndex = -1;
+		secondIndex = -1;
+		for (int i = 0; i < V.rows(); i++) {
+			Eigen::VectorXd minDistance;
+			Eigen::VectorXi prev;
+			igl::dijkstra(i, {}, VV, minDistance, prev);
+			for (int j = 0; j < minDistance.rows(); j++) {
+				if (minDistance(j, 0) > max_distance) {
+					max_distance = minDistance(j, 0);
+					firstIndex = i;
+					secondIndex = j;
+				}
+			}
+		}
+
+		fixed_UV_indices.resize(2, 1);
+		fixed_UV_indices(0, 0) = firstIndex;
+		fixed_UV_indices(1, 0) = secondIndex;
+
+		igl::map_vertices_to_circle(V, fixed_UV_indices, fixed_UV_positions);
+
 	}
 
 	ConvertConstraintsToMatrixForm(fixed_UV_indices, fixed_UV_positions, C, d);
@@ -152,19 +279,51 @@ void computeParameterization(int type)
 	// Find the linear system for the parameterization (1- Tutte, 2- Harmonic, 3- LSCM, 4- ARAP)
 	// and put it in the matrix A.
 	// The dimensions of A should be 2#V x 2#V.
+
+	b.resize(2 * V.rows(), 1);
+	b.setZero();
+
 	if (type == '1') {
 		// Add your code for computing uniform Laplacian for Tutte parameterization
 		// Hint: use the adjacency matrix of the mesh
+		Eigen::SparseMatrix<double> adjacents, adjcentsDiag, U;
+		Eigen::SparseVector<double> adjacentsSum;
+		igl::adjacency_matrix(F, adjacents);
+		igl::sum(adjacents, 1, adjacentsSum);
+		igl::diag(adjacentsSum, adjcentsDiag);
+		U = adjacents - adjcentsDiag;
+
+		igl::repdiag(U, 2, A);
 	}
 
 	if (type == '2') {
 		// Add your code for computing cotangent Laplacian for Harmonic parameterization
 		// Use can use a function "cotmatrix" from libIGL, but ~~~~***READ THE DOCUMENTATION***~~~~
+		Eigen::SparseMatrix<double> L;
+		igl::cotmatrix(V, F, L);
+		igl::repdiag(L, 2, A);
 	}
 
 	if (type == '3') {
 		// Add your code for computing the system for LSCM parameterization
 		// Note that the libIGL implementation is different than what taught in the tutorial! Do not rely on it!!
+		Eigen::SparseMatrix<double> Dx, Dy, Dxt, Dyt, Jr, Jc, J, J1, J2, J3, J4;
+		Eigen::VectorXd Ta;
+
+		computeSurfaceGradientMatrix(Dx, Dy);
+		igl::doublearea(V, F, Ta);
+		Ta /= 2;
+		auto At = Ta.asDiagonal();
+
+		J1 = Dx.transpose() * At * Dx + Dy.transpose() * At * Dy;
+		J2 = Dy.transpose() * At * Dx - Dx.transpose() * At * Dy;
+		J3 = Dx.transpose() * At * Dy - Dy.transpose() * At * Dx;
+		J4 = Dx.transpose() * At * Dx + Dy.transpose() * At * Dy;
+		igl::cat(2, J1, J2, Jr);
+		igl::cat(2, J3, J4, Jc);
+		igl::cat(1, Jr, Jc, J);
+
+		A = J;
 	}
 
 	if (type == '4') {
@@ -177,11 +336,31 @@ void computeParameterization(int type)
 	// Construct the system as discussed in class and the assignment sheet
 	// Use igl::cat to concatenate matrices
 	// Use Eigen::SparseLU to solve the system. Refer to tutorial 3 for more detail
+	Eigen::SparseMatrix<double> N, M, Ct, lhs;
+	VectorXd rhs;
+	Ct = C.transpose();
+	Eigen::SparseMatrix<double> zeros(Ct.cols(), C.rows());
+	igl::cat(1, A, C, N);
+	igl::cat(1, Ct, zeros, M);
+	igl::cat(2, N, M, lhs);
+
+	rhs.resize(b.rows() + d.rows(), 1);
+	rhs << b, d;
+
+	Eigen::SparseLU<SparseMatrix<double>> solver;
+	lhs.makeCompressed();
+	solver.analyzePattern(lhs);
+	solver.factorize(lhs);
+	Eigen::VectorXd res = solver.solve(rhs);
 
 	// The solver will output a vector
 	UV.resize(V.rows(), 2);
 	//UV.col(0) =
 	//UV.col(1) =
+	for (int i = 0; i < V.rows(); i++) {
+		UV(i, 0) = res(i, 0);
+		UV(i, 1) = res(i + V.rows(), 0);
+	}
 }
 
 bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifiers) {
@@ -193,9 +372,27 @@ bool callback_key_pressed(Viewer &viewer, unsigned char key, int modifiers) {
 		computeParameterization(key);
 		break;
 	case '5':
-			// Add your code for detecting and displaying flipped triangles in the
-			// UV domain here
+	{
+		if (angle == true) {
+			distortionType = 0;
+		}
+		else if (edge == true) {
+			distortionType = 1;
+		}
+		else
+		{
+			distortionType = -1;
+		}
+		evaluateDistortion(distortionType);
+
+		distortion = distortion / distortion.maxCoeff();
+		for (int i = 0; i < F.rows(); i++) {
+			distortionColors(i, 0) = 1;
+			distortionColors(i, 1) = 1 - distortion[i];
+			distortionColors(i, 2) = 1 - distortion[i];
+		}
 		break;
+	}
 	case '+':
 		TextureResolution /= 2;
 		break;
@@ -247,7 +444,7 @@ bool callback_init(Viewer &viewer)
 int main(int argc,char *argv[]) {
 	if(argc != 2) {
 		cout << "Usage ex3_bin <mesh.off/obj>" << endl;
-		load_mesh("../data/cathead.obj");
+		load_mesh("C:/Users/orash/Desktop/Computer Science/Geometric Processing/Project/geometryprocessing2021-Orash94/assignment3/data/bunny.off");
 	}
 	else
 	{
@@ -268,6 +465,15 @@ int main(int argc,char *argv[]) {
 		{
 			// Expose variable directly ...
 			ImGui::Checkbox("Free boundary", &freeBoundary);
+			if (ImGui::Button("Angle Preservation")) {
+				angle = true;
+				edge = false;
+			}
+			if (ImGui::Button("Edge Preservation")) {
+				angle = false;
+				edge = true;
+			}
+
 
 			// TODO: Add more parameters to tweak here...
 		}
